95f5fcd052da21423824951ed95d914d18db109b
Simo Sorce <simo@redhat.com>
2022-10-12 22:41:08 -0400

    Simplify containerized CI
    Turns out there is a native github actions command to run a container
    Change the code to avoid additional scripots and pull containers in
    directly in the strategy matrix.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- .github/scripts/run_dockerized.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-COMPILER=${COMPILER:-gcc}
-
-docker run \
-       -v $(pwd):/tmp/build \
-       -w /tmp/build \
-       -e COMPILER=$COMPILER \
-       $DISTRO /bin/bash -ex ./.github/scripts/ci.sh
