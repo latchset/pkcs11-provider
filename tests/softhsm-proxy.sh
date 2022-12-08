09fae38dcdf88793e2f098fa5bdc91a67c493f53
Jakub Jelen <jjelen@redhat.com>
2022-12-08 17:54:09 +0100

    Generate certificates for SoftHSM tests
    This extends the setup similarly as it is done with softokn now to have
    certificates on the smart cards.
    
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- tests/softhsm-proxy.sh
+++ tests/softhsm-proxy.sh
@@ -12,7 +12,9 @@ fi
 title PARA "Start the p11-kit server and check if it works"
 eval $(p11-kit server --provider "$P11LIB" "pkcs11:")
 
-pkcs11-tool -O --login --pin=$PINVALUE --module="$P11KITCLIENTPATH" > /dev/null
+# unset OPENSSL_CONF here to avoid automatically loading the provider
+# and SoftHSM from pkcs11-tool and crashing during the cleanup again.
+OPENSSL_CONF= pkcs11-tool -O --login --pin=$PINVALUE --module="$P11KITCLIENTPATH" > /dev/null
 
 #register clean function to kill p11-kit-server
 trap "cleanup_server p11-kit $P11_KIT_SERVER_PID" EXIT
