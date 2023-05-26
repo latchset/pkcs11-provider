18c6d62c50ae718dfdb467e26a25b72e43198c1f
Jakub Jelen <jjelen@redhat.com>
2023-05-26 10:51:11 +0200

    signature: Avoid using potentially uninitialized variable
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/signature.c
+++ src/signature.c
@@ -1212,6 +1212,10 @@ static int p11prov_rsasig_digest_sign_final(void *ctx, unsigned char *sig,
 {
     P11PROV_SIG_CTX *sigctx = (P11PROV_SIG_CTX *)ctx;
 
+    /* the siglen might be uninitialized when called from openssl */
+    if (siglen) {
+        *siglen = 0;
+    }
     P11PROV_debug(
         "rsa digest sign final (ctx=%p, sig=%p, siglen=%zu, "
         "sigsize=%zu)",
@@ -1853,6 +1857,10 @@ static int p11prov_ecdsa_digest_sign_final(void *ctx, unsigned char *sig,
     size_t rawlen;
     int ret;
 
+    /* the siglen might be uninitialized when called from openssl */
+    if (siglen) {
+        *siglen = 0;
+    }
     P11PROV_debug(
         "ecdsa digest sign final (ctx=%p, sig=%p, siglen=%zu, "
         "sigsize=%zu)",
