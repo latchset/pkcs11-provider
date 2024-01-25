506a2fdaf1c23f9dbafb6abdf4908140d8895c5a
Jakub Jelen <jjelen@redhat.com>
2024-01-25 17:38:54 +0100

    Implement text encoder for Ed25519 keys
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- src/provider.c
+++ src/provider.c
@@ -1083,6 +1083,10 @@ static CK_RV operations_init(P11PROV_CTX *ctx)
     ADD_ALGO_EXT(EC, encoder,
                  "provider=pkcs11,output=der,structure=SubjectPublicKeyInfo",
                  p11prov_ec_encoder_spki_der_functions);
+    ADD_ALGO_EXT(ED25519, encoder, "provider=pkcs11,output=text",
+                 p11prov_ec_edwards_encoder_text_functions);
+    ADD_ALGO_EXT(ED448, encoder, "provider=pkcs11,output=text",
+                 p11prov_ec_edwards_encoder_text_functions);
     if (ctx->encode_pkey_as_pk11_uri) {
         ADD_ALGO_EXT(RSA, encoder,
                      "provider=pkcs11,output=pem,structure=PrivateKeyInfo",
