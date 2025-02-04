92c797face1615b1be0f8114d5d8bbc5cab0ecb4
Simo Sorce <simo@redhat.com>
2025-02-04 15:47:10 -0500

    Add test to sign/verify with generate keypair
    Signed-off-by: Simo Sorce <simo@redhat.com>

---
+++ tests/tpkey.c
@@ -0,0 +1,96 @@
+/* Copyright (C) 2025 Simo Sorce <simo@redhat.com>
+   SPDX-License-Identifier: Apache-2.0 */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/store.h>
+#include <sys/wait.h>
+#include "util.h"
+
+static void sign_op(EVP_PKEY *key, const char *data, size_t len,
+                    unsigned char **signature, size_t *siglen)
+{
+    size_t size = EVP_PKEY_get_size(key);
+    unsigned char *sig;
+    EVP_MD_CTX *sign_md;
+    int ret;
+
+    sig = OPENSSL_zalloc(size);
+    if (!sig) {
+        PRINTERROSSL("Failed to allocate signature buffer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    *signature = sig;
+    *siglen = size;
+
+    sign_md = EVP_MD_CTX_new();
+    ret = EVP_DigestSignInit_ex(sign_md, NULL, "SHA256", NULL, NULL, key, NULL);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to init EVP_DigestSign\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = EVP_DigestSignUpdate(sign_md, data, len);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to EVP_DigestSignUpdate\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = EVP_DigestSignFinal(sign_md, sig, siglen);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to EVP_DigestSignFinal-ize\n");
+        exit(EXIT_FAILURE);
+    }
+    EVP_MD_CTX_free(sign_md);
+}
+
+static void verify_op(EVP_PKEY *key, const char *data, size_t len,
+                      unsigned char *signature, size_t siglen)
+{
+    EVP_MD_CTX *ver_md;
+    int ret;
+
+    ver_md = EVP_MD_CTX_new();
+    ret =
+        EVP_DigestVerifyInit_ex(ver_md, NULL, "SHA256", NULL, NULL, key, NULL);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to init EVP_DigestVerify\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = EVP_DigestVerifyUpdate(ver_md, data, len);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to EVP_DigestVerifyUpdate\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = EVP_DigestVerifyFinal(ver_md, signature, siglen);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to EVP_DigestVerifyFinal-ize/bad signature\n");
+        exit(EXIT_FAILURE);
+    }
+    EVP_MD_CTX_free(ver_md);
+}
+
+int main(int argc, char *argv[])
+{
+    const char *data = "Sign Me!";
+    unsigned char *sig;
+    size_t siglen;
+    EVP_PKEY *key;
+
+    key = util_gen_key("Pkey sigver Test");
+
+    /* test a simple op first */
+    sign_op(key, data, sizeof(data), &sig, &siglen);
+
+    verify_op(key, data, sizeof(data), sig, siglen);
+
+    PRINTERR("ALL A-OK!\n");
+    exit(EXIT_SUCCESS);
+}
--- tests/util.c
+++ tests/util.c
@@ -1,10 +1,13 @@
 /* Copyright (C) 2024 Simo Sorce <simo@redhat.com>
    SPDX-License-Identifier: Apache-2.0 */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdbool.h>
 #include <openssl/err.h>
 #include <openssl/store.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
 #include "util.h"
 
 void ossl_err_print(void)
@@ -138,4 +141,78 @@ X509 *load_cert(const char *uri)
     OSSL_STORE_close(store);
 
     return cert;
+}
+
+void hexify(char *out, unsigned char *byte, size_t len)
+{
+    char c[2], s;
+
+    for (size_t i = 0; i < len; i++) {
+        out[i * 3] = '%';
+        c[0] = byte[i] >> 4;
+        c[1] = byte[i] & 0x0f;
+        for (int j = 0; j < 2; j++) {
+            if (c[j] < 0x0A) {
+                s = '0';
+            } else {
+                s = 'a' - 10;
+            }
+            out[i * 3 + 1 + j] = c[j] + s;
+        }
+    }
+    out[len * 3] = '\0';
+}
+
+EVP_PKEY *util_gen_key(const char *label)
+{
+    unsigned char id[16];
+    char idhex[16 * 3 + 1];
+    char *uri;
+    size_t rsa_bits = 3072;
+    OSSL_PARAM params[3];
+    EVP_PKEY_CTX *ctx;
+    EVP_PKEY *key = NULL;
+    int ret;
+
+    /* RSA */
+    ret = RAND_bytes(id, 16);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to set generate key id\n");
+        exit(EXIT_FAILURE);
+    }
+
+    hexify(idhex, id, 16);
+    ret = asprintf(&uri, "pkcs11:object=%s;id=%s", label, idhex);
+    if (ret == -1) {
+        fprintf(stderr, "Failed to allocate uri\n");
+        exit(EXIT_FAILURE);
+    }
+
+    params[0] = OSSL_PARAM_construct_utf8_string("pkcs11_uri", uri, 0);
+    params[1] = OSSL_PARAM_construct_size_t("rsa_keygen_bits", &rsa_bits);
+    params[2] = OSSL_PARAM_construct_end();
+
+    ctx = EVP_PKEY_CTX_new_from_name(NULL, "RSA", "provider=pkcs11");
+    if (ctx == NULL) {
+        PRINTERROSSL("Failed to init PKEY context\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = EVP_PKEY_keygen_init(ctx);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to init keygen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY_CTX_set_params(ctx, params);
+
+    ret = EVP_PKEY_generate(ctx, &key);
+    EVP_PKEY_CTX_free(ctx);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to generate key\n");
+        exit(EXIT_FAILURE);
+    }
+
+    free(uri);
+    return key;
 }
