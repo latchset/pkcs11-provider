5b4ef9678d183391644c1913ccc34835d45d1835
Simo Sorce <simo@redhat.com>
2025-02-05 15:22:39 -0500

    Add also check for Public Key URI
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- tests/tpkey.c
+++ tests/tpkey.c
@@ -4,7 +4,9 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/evp.h>
 #include <openssl/store.h>
@@ -77,6 +79,41 @@ static void verify_op(EVP_PKEY *key, const char *data, size_t len,
     EVP_MD_CTX_free(ver_md);
 }
 
+static void check_public_info(EVP_PKEY *key)
+{
+    BIO *membio = BIO_new(BIO_s_mem());
+    BUF_MEM *memdata = NULL;
+    const char *type = "type=public";
+    void *found = NULL;
+    int ret;
+
+    if (!membio) {
+        PRINTERROSSL("Failed to instantiate Memory BIO\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = EVP_PKEY_print_public(membio, key, 0, NULL);
+    if (ret != 1) {
+        PRINTERROSSL("Failed to print public key info\n");
+        exit(EXIT_FAILURE);
+    }
+
+    BIO_get_mem_ptr(membio, &memdata);
+    if (!memdata) {
+        PRINTERROSSL("Failed to fetch BIO memory pointer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    found = memmem(memdata->data, memdata->length, type, sizeof(type) - 1);
+    if (!found) {
+        PRINTERR("%.*s\n", (int)memdata->length, memdata->data);
+        PRINTERROSSL("Public type indicator not found in printout!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    BIO_free(membio);
+}
+
 int main(int argc, char *argv[])
 {
     const char *data = "Sign Me!";
@@ -90,6 +127,8 @@ int main(int argc, char *argv[])
     sign_op(key, data, sizeof(data), &sig, &siglen);
 
     verify_op(key, data, sizeof(data), sig, siglen);
+
+    check_public_info(key);
 
     PRINTERR("ALL A-OK!\n");
     exit(EXIT_SUCCESS);
