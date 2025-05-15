329ab8e5d97e585a8fcbbd340d50f286c0af15a9
Ilie Halip <ilie.halip@nxp.com>
2025-05-15 10:59:40 +0300

    fixup! Add support for HMAC.
    Seems clang-format doesn't always give the same result, use the format
    suggested in CI.
    While here, remove an unused macro.
    
    Signed-off-by: Ilie Halip <ilie.halip@nxp.com>

--- src/mac.h
+++ src/mac.h
@@ -7,11 +7,8 @@
 #define _MAC_H
 
 /* MAC fns */
-#define DISPATCH_HMAC_FN(name) DECL_DISPATCH_FUNC(kdf, p11prov_hmac, name)
 #define DISPATCH_MAC_ELEM(prefix, NAME, name) \
-    { \
-        OSSL_FUNC_MAC_##NAME, (void (*)(void))p11prov_##prefix##_##name \
-    }
+    { OSSL_FUNC_MAC_##NAME, (void (*)(void))p11prov_##prefix##_##name }
 extern const OSSL_DISPATCH p11prov_hmac_mac_functions[];
 
 #endif /* _MAC_H */
